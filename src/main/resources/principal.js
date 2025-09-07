bf6dbc7a07f5fefa7d0f39b552d2bcff4f252d34
jhecn <jhecn@murena.io>
2025-09-07 18:13:57 +0200

    séparé en html, css et js

---
+++ src/main/resources/principal.js
@@ -0,0 +1,151 @@
+const svg = document.getElementById("tableau-du-jeu");
+
+const gCellulesTableauDuJeu = document.createElementNS("http://www.w3.org/2000/svg", "g");
+gCellulesTableauDuJeu.setAttribute("id", "cellules-tableau-du-jeu");
+gCellulesTableauDuJeu.setAttribute("fill", "white");
+let nombreDeSecondes = 0;
+let xGaucheTeteSerpent = 0; // 32*6;
+let yGaucheTeteSerpent = 0; // 32*6;
+const intervalId = setInterval(() => {
+    nombreDeSecondes++;
+    console.log("nombreDeSecondes", nombreDeSecondes)
+    if (nombreDeSecondes < 16) {
+        xGaucheTeteSerpent += 32;
+    } else {
+        yGaucheTeteSerpent += 32;
+    }
+    // TODO svg : enlever les 3 enfants corps du serpent, tête du serpent et cellules du tableau
+    svg.appendChild(gCellulesTableauDuJeu);
+    for (let ligneActuelle = 0; ligneActuelle < 16; ligneActuelle++) {
+        for (let colonneActuelle = 0; colonneActuelle < 16; colonneActuelle++) {
+            let x = 32 * ligneActuelle;
+            let y = 32 * colonneActuelle;
+            const rectCellule = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+            rectCellule.setAttribute("x", x.toString());
+            rectCellule.setAttribute("y", y.toString());
+            rectCellule.setAttribute("width", "32");
+            rectCellule.setAttribute("height", "32");
+            gCellulesTableauDuJeu.appendChild(rectCellule);
+        }
+    }
+
+    const gCorpsDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    gCorpsDuSerpent.setAttribute("id", "corps-du-serpent");
+    const rectCorpsDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rectCorpsDuSerpent.setAttribute("x", (xGaucheTeteSerpent - 32).toString());
+    rectCorpsDuSerpent.setAttribute("y", yGaucheTeteSerpent);
+    rectCorpsDuSerpent.setAttribute("width", "32");
+    rectCorpsDuSerpent.setAttribute("height", "32");
+    rectCorpsDuSerpent.setAttribute("fill", "green");
+    gCorpsDuSerpent.appendChild(rectCorpsDuSerpent);
+    svg.appendChild(gCorpsDuSerpent);
+
+    const gTeteDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    gTeteDuSerpent.setAttribute("id", "tete-du-serpent");
+    svg.appendChild(gTeteDuSerpent);
+
+    const rect1TeteDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect1TeteDuSerpent.setAttribute("x", xGaucheTeteSerpent.toString());
+    rect1TeteDuSerpent.setAttribute("y", yGaucheTeteSerpent);
+    rect1TeteDuSerpent.setAttribute("width", "32");
+    rect1TeteDuSerpent.setAttribute("height", "32");
+    rect1TeteDuSerpent.setAttribute("fill", "white");
+    gTeteDuSerpent.appendChild(rect1TeteDuSerpent);
+
+    const circleTeteDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circleTeteDuSerpent.setAttribute("cx", (xGaucheTeteSerpent + 8).toString());
+    circleTeteDuSerpent.setAttribute("cy", (yGaucheTeteSerpent + 16).toString());
+    circleTeteDuSerpent.setAttribute("r", "16");
+    circleTeteDuSerpent.setAttribute("fill", "green");
+    gTeteDuSerpent.appendChild(circleTeteDuSerpent);
+
+    const rect2TeteDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect2TeteDuSerpent.setAttribute("x", xGaucheTeteSerpent.toString());
+    rect2TeteDuSerpent.setAttribute("y", yGaucheTeteSerpent);
+    rect2TeteDuSerpent.setAttribute("width", "8");
+    rect2TeteDuSerpent.setAttribute("height", "32");
+    rect2TeteDuSerpent.setAttribute("fill", "green");
+    gTeteDuSerpent.appendChild(rect2TeteDuSerpent);
+
+    const oeilHautTeteDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    oeilHautTeteDuSerpent.setAttribute("cx", (xGaucheTeteSerpent + 8).toString());
+    oeilHautTeteDuSerpent.setAttribute("cy", (yGaucheTeteSerpent + 8).toString());
+    oeilHautTeteDuSerpent.setAttribute("r", "3");
+    oeilHautTeteDuSerpent.setAttribute("fill", "white");
+    gTeteDuSerpent.appendChild(oeilHautTeteDuSerpent);
+
+    const pupilleHautTeteDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    pupilleHautTeteDuSerpent.setAttribute("cx", (xGaucheTeteSerpent + 8).toString());
+    pupilleHautTeteDuSerpent.setAttribute("cy", (yGaucheTeteSerpent + 8).toString());
+    pupilleHautTeteDuSerpent.setAttribute("r", "2");
+    pupilleHautTeteDuSerpent.setAttribute("fill", "black");
+    gTeteDuSerpent.appendChild(pupilleHautTeteDuSerpent);
+
+    const oeilBasTeteDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    oeilBasTeteDuSerpent.setAttribute("cx", (xGaucheTeteSerpent + 8).toString());
+    oeilBasTeteDuSerpent.setAttribute("cy", (yGaucheTeteSerpent + 24).toString());
+    oeilBasTeteDuSerpent.setAttribute("r", "3");
+    oeilBasTeteDuSerpent.setAttribute("fill", "white");
+    gTeteDuSerpent.appendChild(oeilBasTeteDuSerpent);
+
+    const pupilleBasTeteDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    pupilleBasTeteDuSerpent.setAttribute("cx", (xGaucheTeteSerpent + 8).toString());
+    pupilleBasTeteDuSerpent.setAttribute("cy", (yGaucheTeteSerpent + 24).toString());
+    pupilleBasTeteDuSerpent.setAttribute("r", "2");
+    pupilleBasTeteDuSerpent.setAttribute("fill", "black");
+    gTeteDuSerpent.appendChild(pupilleBasTeteDuSerpent);
+
+    const langueDuSerpent = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    let texteDuDlangueDuSerpent = "M" + (xGaucheTeteSerpent + 31).toString() +
+        " " + (yGaucheTeteSerpent + 19).toString() +
+        " L" + (xGaucheTeteSerpent + 29).toString() +
+        " " + (yGaucheTeteSerpent + 17).toString() +
+        " L" + (xGaucheTeteSerpent + 31).toString() +
+        " " + (yGaucheTeteSerpent + 15).toString() +
+        " M" + (xGaucheTeteSerpent + 29).toString() +
+        " " + (yGaucheTeteSerpent + 17).toString() +
+        " L" + (xGaucheTeteSerpent + 19).toString() +
+        " " + (yGaucheTeteSerpent + 17).toString()
+    langueDuSerpent.setAttribute("d", texteDuDlangueDuSerpent);
+    langueDuSerpent.setAttribute("stroke", "red");
+    langueDuSerpent.setAttribute("stroke-width", "1.5");
+    langueDuSerpent.setAttribute("fill", "none");
+    gTeteDuSerpent.appendChild(langueDuSerpent);
+
+    if (nombreDeSecondes >= 16) {
+        let transformation = "rotate(90, " + (xGaucheTeteSerpent + 16) + ", "
+            + (yGaucheTeteSerpent + 16).toString() + ")";
+        gCorpsDuSerpent.setAttribute("transform", transformation);
+        gTeteDuSerpent.setAttribute("transform", transformation);
+    }
+}, 1000); // 1000ms = 1 seconde
+setTimeout(() => {
+    clearInterval(intervalId);
+}, 30900);
+
+
+// const gLignesVerticales = document.createElementNS("http://www.w3.org/2000/svg", "g");
+// gLignesVerticales.setAttribute("id","lignes-verticales-du-quadrillage");
+// svg.appendChild(gLignesVerticales);
+// for (let colonneActuelle = 0; colonneActuelle < 17; colonneActuelle++) {
+//     let x = 32*colonneActuelle;
+//     let texteDuD = "M" + x + ", 0 L" + x + ", 512"
+//     const ligneVerticale = document.createElementNS("http://www.w3.org/2000/svg", "path");
+//     ligneVerticale.setAttribute("d",texteDuD);
+//     ligneVerticale.setAttribute("stroke","black");
+//     ligneVerticale.setAttribute("stroke-width","1");
+//     gLignesVerticales.appendChild(ligneVerticale);
+// }
+//
+// const gLignesHorizontales = document.createElementNS("http://www.w3.org/2000/svg", "g");
+// gLignesHorizontales.setAttribute("id","lignes-horizontales-du-quadrillage");
+// svg.appendChild(gLignesHorizontales);
+// for (let ligneHorizontale = 0; ligneHorizontale < 17; ligneHorizontale++) {
+//     let y = 32*ligneHorizontale;
+//     let texteDuD = "M0, " + y + " L512, " + y
+//     const ligneHorizontalePath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+//     ligneHorizontalePath.setAttribute("d",texteDuD);
+//     ligneHorizontalePath.setAttribute("stroke","black");
+//     ligneHorizontalePath.setAttribute("stroke-width","1");
+//     gLignesHorizontales.appendChild(ligneHorizontalePath);
+// }
